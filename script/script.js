d3d10e89ed23994cbe8c7960dde3742536d97a47
chevonnelise <elise.chevonne@gmail.com>
2023-01-10 12:36:43 +0000

    initialise map

--- script/script.js
+++ script/script.js
@@ -1,13 +1,20 @@
-var franceMap = L.map('map').setView([46.2276, 2.2137], 13);
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+let france = [46.2276, 2.2137];
+let map = L.map("map").setView(france, 19);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox/streets-v11',
+  tileSize: 512,
+  zoomOffset: -1,
+  accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
 }).addTo(map);
 
-// script
-const element = document.getElementById("welcome-btn");
-    element.addEventListener("click", function(){
-    document.getElementById("frenchWines").innerHTML = "Discover the wines across France";
-});
+var vineyardIcon = L.icon({
+    iconURL: "../images/grape.png", 
+    iconSize: [20,20]
+}); 
+
+
 
     
