a771115fc6055a9a41d29f7ebad06168c5d99272
chevonnelise <elise.chevonne@gmail.com>
2023-01-10 13:51:34 +0000

    add accordion

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 let france = [46.2276, 2.2137];
-let map = L.map("map").setView(france, 19);
+let map = L.map("map",{scrollWheelZoom: false}).setView(france, 9);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
